d3f7de85a14f2879e4d98ca5da18362d774b46de
SonTV <son.tv@shb.com.vn>
2022-12-23 00:17:24 +0700

    update list project portfolio

--- constants/projects.ts
+++ constants/projects.ts
@@ -1,35 +1,23 @@
 import { ProjectProp } from "../components/ProjectItem";
-import weatherImg from "../public/assets/projects/weather.png";
-import foodImg from "../public/assets/projects/food-app.png";
 import vmoImg from "../public/assets/projects/vmo-app.png";
 import natureImg from "../public/assets/projects/nature-app.png";
 import blogImg from "../public/assets/projects/blog-app.png";
 import bankImg from "../public/assets/projects/bank-LP.png";
+import goenImg from "../public/assets/projects/goen.png";
+import marketPlaceImg from "../public/assets/projects/marketplace.png";
 
 export const LIST_PROJECT: ProjectProp[] = [
   {
-    title: "Weather App",
-    bgImg: weatherImg,
-    url: "https://react-weather-app-22019.web.app/",
-    skills: "Reactjs, TailwindCSS",
+    title: "Goen Finance",
+    bgImg: goenImg,
+    url: "https://www.goen.finance/",
+    skills: "Vuejs, TailwindCSS, Scss",
   },
   {
-    title: "Shopping App",
-    bgImg: foodImg,
-    url: "https://shopping-food-drink.web.app/",
-    skills: "Reactjs, Redux",
-  },
-  {
-    title: "Blog and Portfolio",
-    bgImg: blogImg,
-    url: "https://sontranportfolio.netlify.app/post",
-    skills: "Reactjs, TailwindCSS",
-  },
-  {
-    title: "Mini Projects Training",
-    bgImg: vmoImg,
-    url: "https://login-eec04.web.app",
-    skills: "Reactjs, TailwindCSS, Redux",
+    title: "Marketplace NFT",
+    bgImg: marketPlaceImg,
+    url: "https://doragonland.mirana.market/",
+    skills: "Reactjs, Redux, TailwindCSS",
   },
   {
     title: "Landing Page Nature",
@@ -42,5 +30,17 @@ export const LIST_PROJECT: ProjectProp[] = [
     bgImg: bankImg,
     url: "https://bank-landing-page-hbo.vercel.app/",
     skills: "Reactjs, TailwindCSS, Vite",
+  },
+  {
+    title: "Blog and Portfolio",
+    bgImg: blogImg,
+    url: "https://sontranportfolio.netlify.app/post",
+    skills: "Reactjs, TailwindCSS",
+  },
+  {
+    title: "Mini Projects Training",
+    bgImg: vmoImg,
+    url: "https://login-eec04.web.app",
+    skills: "Reactjs, TailwindCSS, Redux",
   },
 ];
